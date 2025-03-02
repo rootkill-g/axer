c0ebb5365454c87ea4794dd63c580dacc4141cc1
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-03-02 22:40:59 +0530

    Added test for root route

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -4,3 +4,18 @@ use actix_web::{get, HttpResponse, Responder};
 pub async fn root() -> impl Responder {
     HttpResponse::Ok().body("INDEX=>HIT_SUCCESS")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, App};
+
+    #[actix_web::test]
+    async fn test_route_root() {
+        let app = test::init_service(App::new().service(root)).await;
+        let req = test::TestRequest::get().uri("/").to_request();
+        let res = test::call_service(&app, req).await;
+
+        assert!(res.status().is_success())
+    }
+}
