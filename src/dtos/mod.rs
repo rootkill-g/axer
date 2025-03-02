c1b4c466dc32f6fdefbb1b96f336010514b228e4
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-03-02 22:40:22 +0530

    Added DTOs for register request and response for wasm module registration APIs

--- src/dtos/mod.rs
+++ src/dtos/mod.rs
@@ -1,9 +1,21 @@
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Clone, FromRow)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WasmModuleRegisterRequest {
+    pub name: String,
+    pub wasm: Vec<u8>,
+}
+
+#[derive(Debug, Serialize, Clone, PartialEq, FromRow)]
 pub struct WasmModule {
     pub id: String,
     pub name: String,
     pub wasm: Vec<u8>,
+}
+
+#[derive(Debug, Serialize, FromRow)]
+pub struct WasmModuleResponse {
+    pub id: String,
+    pub name: String,
 }
