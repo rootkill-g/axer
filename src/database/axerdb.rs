97168c257c6aeee405785b0451628c4aabb8f783
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-03-02 17:20:33 +0530

    implementing database layer using sqlite driver from sqlx, updated schemas handling and efficient queries and unit testing

--- src/database/axerdb.rs
+++ src/database/axerdb.rs
@@ -1,100 +1,95 @@
-use rusqlite::{params, Connection, Result as AxerDBResult};
-use ulid::Ulid;
+use sqlx::{self, Result as AxerDBResult, SqlitePool};
 
 use crate::dtos::WasmModule;
 
-pub struct AxerDB(Connection);
+pub struct AxerDBPool(SqlitePool);
 
-impl AxerDB {
-    pub fn new() -> Self {
-        let mut conn = Connection::open("axer.db")
-            .expect("FATAL: Error occured while initializing database! ABORTING");
+impl AxerDBPool {
+    pub async fn new() -> AxerDBPool {
+        let cpool = SqlitePool::connect("sqlite://axer.db")
+            .await
+            .expect("Failed to connect to database");
 
-        conn.set_transaction_behavior(rusqlite::TransactionBehavior::Immediate);
-
-        conn.execute(
-            "
-                CREATE TABLE IF NOT EXISTS wasm_modules (
-                    id TEXT PRIMARY KEY,
-                    module_name TEXT NOT NULL,
-                    mime_type TEXT NOT NULL,
-                    bin_data BLOB NOT NULL
-                )
-            ",
-            [],
-        )
-        .expect("Failed to create database table");
-
-        AxerDB(conn)
+        AxerDBPool(cpool)
     }
 
-    pub fn write(&self, wasm_module: WasmModule) -> AxerDBResult<()> {
-        let module_id = Ulid::new().to_string();
-
-        let mut cached_statement = self.0.prepare_cached(
+    pub async fn write(&self, wasm_module: WasmModule) -> AxerDBResult<String> {
+        match sqlx::query(
             "
-                INSERT INTO wasm_modules 
-                (id, module_name, mime_type, bin_data)
-                VALUES (?1,  ?2, ?3, ?4)
-            ",
-        )?;
+            INSERT INTO wasm_modules (id, name, wasm)
+            VALUES (?, ?, ?)
+            RETURNING name
+        ",
+        )
+        .bind(wasm_module.id)
+        .bind(wasm_module.name.clone())
+        .bind(wasm_module.wasm)
+        .execute(&self.0)
+        .await
+        {
+            Ok(_) => Ok(wasm_module.name),
+            Err(err) => Err(err),
+        }
+    }
 
-        cached_statement.execute(params![
-            module_id,
-            wasm_module.module_name,
-            wasm_module.mime_type,
-            wasm_module.bin_data
-        ])?;
-
-        Ok(())
+    pub async fn read(&self, module_id: String) -> AxerDBResult<WasmModule> {
+        Ok(sqlx::query_as(
+            "
+            SELECT id, name, wasm
+            FROM wasm_modules
+            WHERE id = ?
+        ",
+        )
+        .bind(&module_id)
+        .fetch_one(&self.0)
+        .await
+        .unwrap())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use sqlx::Row;
+    use ulid::Ulid;
 
-    #[test]
-    fn test_init_axer_db() {
-        let axerdb = AxerDB::new();
-        let mut row = axerdb
-            .0
-            .prepare("SELECT id, module_name, mime_type, bin_data FROM wasm_modules")
-            .expect("Failed to get rows from table");
+    #[tokio::test]
+    async fn test_init_axer_db() {
+        let cpool = AxerDBPool::new().await;
+        let query = "SELECT COUNT(*) FROM wasm_modules";
 
-        assert_eq!(row.execute(()), Ok(0));
+        let res = sqlx::query(query).fetch_one(&cpool.0).await.unwrap();
+
+        assert!(!res.is_empty());
     }
 
-    #[test]
-    fn test_write() {
-        let axerdb = AxerDB::new();
+    #[tokio::test]
+    async fn test_write() {
+        let cpool = AxerDBPool::new().await;
         let wat_bytes =
             b"(module (func (export \"test\") (param i32) (result i32) local.get 0))".to_vec();
         let wasm_module = WasmModule {
             id: Ulid::new().to_string(),
-            module_name: String::from("test_module"),
-            mime_type: String::from("wasm"),
-            bin_data: wat_bytes,
+            name: String::from("test_module").into(),
+            wasm: wat_bytes,
         };
 
-        assert_eq!(axerdb.write(wasm_module), Ok(()));
+        assert!(cpool.write(wasm_module).await.is_ok());
     }
 
-    #[test]
-    fn test_write_and_read() {
-        let axerdb = AxerDB::new();
+    #[tokio::test]
+    async fn test_write_and_read() {
+        let cpool = AxerDBPool::new().await;
         let wat_bytes =
             b"(module (func (export \"test\") (param i32) (result i32) local.get 0))".to_vec();
         let module_id = Ulid::new().to_string();
         let wasm_module = WasmModule {
-            id: module_id,
-            module_name: String::from("test_module"),
-            mime_type: String::from("wasm"),
-            bin_data: wat_bytes,
+            id: module_id.clone(),
+            name: String::from("test_module").into(),
+            wasm: wat_bytes,
         };
 
-        assert_eq!(axerdb.write(wasm_module), Ok(()));
-
-        assert_eq!(axerdb.read(module_id), Ok(wasm_module));
+        assert!(cpool.write(wasm_module.clone()).await.is_ok());
+        assert!(cpool.read(module_id.into()).await.is_ok());
     }
 }
